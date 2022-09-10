4ab09f1f4efad0ca7752124c84ca8545f8a3db0d
tatitrif <64037685+tatitrif@users.noreply.github.com>
2022-09-10 23:51:57 +0300

    add partition

--- auth/src/db/relational/extensions.py
+++ auth/src/db/relational/extensions.py
@@ -14,7 +14,6 @@ def init_db(app: Flask) -> None:
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config.sqlalchemy_track_modifications
     db.init_app(app)
     app.app_context().push()
-    db.create_all()
 
 
 def init_migrate(app: Flask) -> None:
