24b123fa5d716314456f03dc79ee71c07324c897
Ефремов Владислав Александрович <VAlEfremov@sberbank.ru>
2022-09-10 19:56:37 +0300

    refactor: rate limit

--- auth/src/core/utils.py
+++ auth/src/core/utils.py
@@ -16,21 +16,20 @@ def rate_limit(limit=RATE_LIMIT, interval=RATE_INTERVAL):
     def rate_limit_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            key: str = f"Limit::{request.remote_addr}:{datetime.datetime.now().minute}"
-            current_request_count = cache.get(key=key)
+            key = f"limit::{request.remote_addr}:{datetime.datetime.now().minute}"
+            current_requests_count = cache.get(key=key)
 
-            if current_request_count and int(current_request_count) >= limit:
+            if current_requests_count and int(current_requests_count) >= limit:
                 return {
-                    "message": f"Too many requests. Limit {limit} in {interval} seconds",
-                }, HTTPStatus.TOO_MANY_REQUESTS
+                           "message": f"Too many requests. Limit {limit} in {interval} seconds",
+                       }, HTTPStatus.TOO_MANY_REQUESTS
 
-            else:
-                pipe = cache.pipeline()
-                pipe.incr(key, 1)
-                pipe.expire(key, interval + 1)
-                pipe.execute()
+            pipe = cache.pipeline()
+            pipe.incr(key, 1)
+            pipe.expire(key, interval + 1)
+            pipe.execute()
 
-                return func(*args, **kwargs)
+            return func(*args, **kwargs)
 
         return wrapper
 
