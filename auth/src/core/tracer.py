fb31bfea7abfb4b2332d5eea6c68bb7393bbcb03
tatitrif <64037685+tatitrif@users.noreply.github.com>
2022-09-11 18:52:10 +0300

    fix: tracer console only for debug

--- auth/src/core/tracer.py
+++ auth/src/core/tracer.py
@@ -1,9 +1,11 @@
 from core.config import config
+from flask import Flask
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 
 def configure_tracer() -> None:
@@ -17,4 +19,11 @@ def configure_tracer() -> None:
         )
     )
     # Чтобы видеть трейсы в консоли
-    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+    if config.DEBUG:
+        trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+
+
+def init_tracer(app: Flask) -> None:
+    configure_tracer()
+    FlaskInstrumentor().instrument_app(app)
+
